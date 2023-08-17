51a56c18a46eec93b3c11ec9894065365891bf01
Cristhian Garcia <crisgarta8@gmail.com>
2023-08-17 14:56:28 -0500

    test: fix tests settings

--- event_sink_clickhouse/utils.py
+++ event_sink_clickhouse/utils.py
@@ -3,11 +3,18 @@ from importlib import import_module
 from django.conf import settings
 
 
-def get_model(model_name):
-    model_config = getattr(settings, "EVENT_SINK_CLICKHOUSE_MODEL_CONFIG").get(
-        model_name
-    )
-    module = model_config["module"]
-    model_name = model_config["model"]
+def get_model(model_setting):
+    MODEL_CONFIG = getattr(settings, "EVENT_SINK_CLICKHOUSE_MODEL_CONFIG", {})
+
+    model_config = getattr(MODEL_CONFIG, model_setting, {})
+
+    module = model_config.get("module")
+    if not module:
+        return None
+
+    model_name = model_config.get("model")
+    if not model_name:
+        return None
+
     model = getattr(import_module(module), model_name)
     return model
